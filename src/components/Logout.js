149270d7b8d836585c5a1f672e4fe466ec018fa2
jenitamary0321 <jenitamary0321@gmail.com>
2024-10-09 11:59:09 +0530

    commit

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,19 +1,13 @@
-import {useNavigate} from 'react-router-dom';
+import { useEffect } from 'react';
 import eventEmitter from './eventEmitter';
 
 const Logout = () => {
-  const navigate  = useNavigate();
-
-  const handleLogout = (() => {
-  
-    localStorage.removeItem('authToken');  
-    eventEmitter.emit("message", "false");
-     
-    window.location='./login'
-  })();    
-
-  
+  useEffect(() => {
+  localStorage.removeItem('authToken');
+   eventEmitter.emit("message", "false");
+    window.location.href = './login';
+  }, []);  
+  return null;  
 };
 
 export default Logout;
- 
